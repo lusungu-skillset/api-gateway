8e09244eba546cc7a451e2374cf042ec5af2a332
Lusungu Mhango <lusungu-skillset>
2025-08-27 15:45:45 +0200

    updated cors

--- src/main.ts
+++ src/main.ts
@@ -16,7 +16,7 @@ async function bootstrap() {
 
   await app.startAllMicroservices();
     app.enableCors({
-    origin: ["http://localhost:5173"], 
+    origin: "*", 
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   });
